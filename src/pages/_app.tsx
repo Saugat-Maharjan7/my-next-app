a57c8105dbc1942a23bb8cb53022593db383ced5
Saugat Maharjan <saugatmaharjan11@gmail.com>
2025-04-28 20:10:03 +0545

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import "../style-dictionary-dist/variables.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
